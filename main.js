d691b7735edd9d2cfcd500b5db9e1a69beb693a0
David Törnros <david@dxgruppen.se>
2023-11-06 13:02:26 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,104 @@
+var map = L.map('map').fitWorld();
+var pendingQuery = null;
+var processingQuery = null;
+var routes={};
+let drawnRelations = [];
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom:19,
+    minZoom:11,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+
+// Defaul location (Örebro)
+map.setView([59.28, 15.229], 12);
+
+// Locate the user
+map.locate({setView: true});
+
+// When user is located, add marker
+map.on('locationfound', function(e){
+    console.log("User located.")
+    var radius = e.accuracy;
+    L.marker(e.latlng).addTo(map);
+    L.circle(e.latlng, radius).addTo(map);
+    createPendingQuery();
+});
+map.on('locationerror', function(e) {
+    console.log("User not located.");
+    createPendingQuery();
+  });
+
+map.on('moveend', function (e){
+    createPendingQuery();
+});
+
+
+
+function processPendingQuery() {
+  if (pendingQuery!=null) {
+    if (processingQuery===null) {
+        console.log("Fetching map data")
+        processingQuery=pendingQuery;
+        pendingQuery = null;
+        fetch('https://overpass.kumi.systems/api/interpreter', {
+            method: 'POST',
+            body: processingQuery
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("Data fetched");
+                processingQuery = null;
+
+                const relations = data.elements.filter(element => element.type === 'relation');
+                const ways = data.elements.filter(element => element.type === 'way');
+                const nodes = data.elements.filter(element => element.type === 'node');
+                
+
+                relations.forEach(relation => {
+                    if (routes[relation.id]) return;
+                    newRoute = new Route(relation);
+                    relation.members.forEach(member => {
+                        const way = ways.find(way => way.id === member.ref);
+                        if (way) {
+                            way["coordinates"] = [];
+                            way.nodes.forEach(nodeID => {
+                                const node = nodes.find(node => node.id === nodeID);
+                                way["coordinates"].push([node.lat, node.lon]);
+                            }); 
+                            newRoute.addWay(way);
+                    }
+                       
+                    });
+          
+                    routes[relation.id]= newRoute;
+                    newRoute.addToMap(map);
+
+                           
+
+            });
+        }).catch(error => {
+            console.error("Error fetching data:", error);
+            processingQuery = null;
+        });;
+        
+ 
+}}}
+
+// Call the function every 10 seconds to make sure to deal with pending requests
+setInterval(processPendingQuery, 10000);
+
+
+function createPendingQuery(){
+    var bounds = map.getBounds();
+    var southWest = bounds.getSouthWest();
+    var northEast = bounds.getNorthEast();
+     pendingQuery = `[out:json];
+    (
+      relation["type"="route"]["route"="hiking"](${southWest.lat}, ${southWest.lng}, ${northEast.lat}, ${northEast.lng});
+    );
+    out body;
+    >;
+out skel qt;`;
+processPendingQuery();
+}
---
+++ route.js
@@ -0,0 +1,33 @@
+class Route {
+    constructor(relation) {
+        this.name=relation.tags.name;
+        this.id=relation.id;
+        this.ways=[];
+        this.marker = null;
+        console.log(`Route ${this.name} added`);
+    }
+
+    addWay(way){
+        this.ways.push(way);
+    }
+
+    // Function, Add route graphics to Map
+    addToMap(map) {
+        // Add a marker 
+        this.marker= L.marker([this.ways[0]["coordinates"][0][0], this.ways[0]["coordinates"][0][1]]).addTo(map);
+        this.marker.bindTooltip('Namn: ' + this.name ).openPopup();
+
+        // Loop the ways
+       this.ways.forEach(way => {
+        // Draw the path with a random color based on the ID
+        const pathPolyline = L.polyline(way["coordinates"]).addTo(map);
+        const red = (this.id * 15) % 256;
+        const green = (this.id * 31) % 256;
+        const blue = (this.id * 10) % 256;
+        const color = `rgb(${red}, ${green}, ${blue})`;
+        pathPolyline.setStyle({ color: color, weight: 4 });
+    });
+    
+    }
+
+}
